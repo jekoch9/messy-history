bc133a2f64d42bb2b45c752cbfcefed215990255
james.koch <james.koch@slalom.com>
2024-02-04 13:27:29 -0500

    added function

--- messy-history.js
+++ messy-history.js
@@ -10,3 +10,26 @@ export function addValues(a, b) {
   //A: Because it doesn't add any value to the code
   return a + b;
 }
+//reads in a csv file and uses the helpers to perform the math operations
+import { sum, subtract, multiply, divide } from "./helpers/helper.js";
+const fs = require("fs");
+const path = require("path");
+const helpers = require("./helpers/helper.js");
+const csv = require("csv-parser");
+const results = [];
+
+//The CSV file is at the root of the project
+const csvPath = path.join(__dirname, "math.csv");
+
+//Function to read the CSV file
+export const readCSV = () => {
+  fs.createReadStream(csvPath)
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      console.log("CSV file successfully processed");
+    });
+};
+
+
+
